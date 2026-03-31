47fe8b5425e95634cf186851fa5d614cd2e25555
mikeangelo1212 <bardojimenez1212@gmail.com>
2026-03-30 22:50:30 -0500

    agregar putUser, ahora si edita el pin

--- src/Infrastructure/Data/Configurations/UserConfiguration.cs
+++ src/Infrastructure/Data/Configurations/UserConfiguration.cs
@@ -9,8 +9,8 @@ public class UserConfiguration : IEntityTypeConfiguration<User>
     public void Configure(EntityTypeBuilder<User> builder)
     {
         builder.ToTable("host_user");
-        builder.HasKey(t => t.Id).HasName("host_user_pkey");
-        builder.HasAlternateKey(t => t.Pin).HasName("host_user_pin_key");
+        // builder.HasKey(t => t.Id).HasName("host_user_pkey");
+        // builder.HasAlternateKey(t => t.Pin).HasName("host_user_pin_key");
         builder.HasIndex(t => t.Pin).IsUnique().HasDatabaseName("host_user_pin_key");
 
         builder.Property(t => t.Id).HasColumnName("id").IsRequired();
